efca58605f6ea80a626dafa38f61915187cd2ac5
Sandeep <sandeepreyyi2003@gmail.com>
2025-09-11 17:01:24 +0530

    apis are chenged

--- BackEnd/dist/studentbookfrontend/urls/user_dashboard_urls.py
+++ BackEnd/dist/studentbookfrontend/urls/user_dashboard_urls.py
@@ -4,7 +4,7 @@ from studentbookfrontend.views.user_dashboard_views import *
 
 urlpatterns = [
     path("<int:student_id>/<int:class_id>", DashboardAPIView.as_view()),
-    path("<int:student_id>/<int:class_id>/topic-interest", TopicInterestChartAPIView.as_view()),
-    path("<int:student_id>/<int:class_id>/weekly-trends", WeeklyLearningTrendsAPI.as_view()),
+    path("topic-interest/<int:student_id>/<int:class_id>", TopicInterestChartAPIView.as_view()),
+    path("weekly-trends/<int:student_id>/<int:class_id>", WeeklyLearningTrendsAPI.as_view()),
     # path("subject-list/<int:student_id>/<int:class_id>", SubjectList.as_view()),
 ]
